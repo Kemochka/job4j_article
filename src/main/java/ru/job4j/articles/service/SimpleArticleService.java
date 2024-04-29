af1ae8ef6825bdeec4075063ef81d2fd9eb3f999
kemochka <kemaevak@yandex.ru>
2024-04-29 17:59:00 +0300

    3. GC. Out Of Mem. Тестовое задание. [#504790 #493530]

--- src/main/java/ru/job4j/articles/service/SimpleArticleService.java
+++ src/main/java/ru/job4j/articles/service/SimpleArticleService.java
@@ -6,9 +6,7 @@ import ru.job4j.articles.model.Article;
 import ru.job4j.articles.model.Word;
 import ru.job4j.articles.service.generator.ArticleGenerator;
 import ru.job4j.articles.store.Store;
-import java.lang.ref.WeakReference;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.stream.IntStream;
 
 public class SimpleArticleService implements ArticleService {
 
@@ -22,19 +20,11 @@ public class SimpleArticleService implements ArticleService {
 
     @Override
     public void generate(Store<Word> wordStore, int count, Store<Article> articleStore) {
-        LOGGER.info("Геренация статей в количестве {}", count);
+        LOGGER.info("Генерация статей в количестве {}", count);
         var words = wordStore.findAll();
-        List<WeakReference<Article>> articles = new ArrayList<>();
-        for (int i = 0; i < count; i++) {
-            LOGGER.info("Сгенерирована статья № {}", i);
-            Article article = articleGenerator.generate(words);
-            articles.add(new WeakReference<>(article));
-        }
-        for (WeakReference<Article> weakArticle : articles) {
-            Article article = weakArticle.get();
-            if (article != null) {
-                articleStore.save(article);
-            }
-        }
+        IntStream.iterate(0, i -> i < count, i -> i + 1)
+                .peek(i -> LOGGER.info("Сгенерирована статья № {}", i))
+                .mapToObj((x) -> articleGenerator.generate(words))
+                .forEach(articleStore::save);
     }
 }
